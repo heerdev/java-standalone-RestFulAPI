1c1dbaa245d208f65743b4e66d9090690a679e6f
zubair <zubair@heersoftware.com>
2019-03-16 23:25:10 +0530

    initial DB

--- moneytransfer/src/main/java/MoneyTransferApp.java
+++ moneytransfer/src/main/java/MoneyTransferApp.java
@@ -2,6 +2,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import factory.DBProvider;
+import model.BankLocation;
+import org.codehaus.jackson.map.ObjectMapper;
+
 import java.io.*;
 import java.net.InetSocketAddress;
 
@@ -18,6 +21,7 @@ public class MoneyTransferApp {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/test", new MyHandler());
+        server.createContext("/bank-locations", new GetBankLocation());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
@@ -27,6 +31,22 @@ public class MoneyTransferApp {
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class GetBankLocation implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+
+            ObjectMapper mapper = new ObjectMapper();
+            BankLocation bankLocation= new BankLocation();
+            bankLocation.setBranchId(001);
+            bankLocation.setAddress("New York City");
+            //Object to JSON in String
+            String jsonInString = mapper.writeValueAsString(bankLocation);
+            t.sendResponseHeaders(200, jsonInString.length());
+            OutputStream os = t.getResponseBody();
+            os.write(jsonInString.getBytes());
             os.close();
         }
     }
---
+++ moneytransfer/src/main/java/model/BankLocation.java
@@ -0,0 +1,23 @@
+package model;
+
+public class BankLocation {
+
+    private int branchId;
+    private String address;
+
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(int branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
