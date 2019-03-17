00aa621bab156d5b197b94799741895cded5c563
zubair <zubair@heersoftware.com>
2019-03-17 23:18:18 +0530

    payment_transaction - fun begin!!

--- moneytransfer/src/main/java/util/db/DBSchema.java
+++ moneytransfer/src/main/java/util/db/DBSchema.java
@@ -21,7 +21,7 @@ public class  DBSchema {
     public static final String accounts="CREATE TABLE  ACCOUNTS (account_id INTEGER  not null, customer_id INTEGER  not null,account_type_id INTEGER  not null,account_nmer int,balance float,PRIMARY KEY (account_id))";
     public static final String accountType="CREATE TABLE  ACCOUNT_TYPE (account_type_id INTEGER  not null, account_type varchar(255),PRIMARY KEY (account_type_id))";
 
-    public static final String paymentTransaction="CREATE TABLE PAYMENT_TRANSACTION(pymt_txn_id INTEGER  not null,cr_account INTEGER not null,dr_account INTEGER not null, sender_ref varchar(255),txn_dt Date not null,amount float not null,currency varchar(255) not null, transfer_type varchar(255) not null, CrDr varchar(10) not null)";
+    public static final String paymentTransaction="CREATE TABLE PAYMENT_TRANSACTION(pymt_txn_id INTEGER  not null,cr_account INTEGER not null,dr_account INTEGER not null, sender_ref varchar(255),txn_dt Date not null,amount float not null,currency varchar(255) not null, transfer_type varchar(255) not null, CrDr varchar(10),PRIMARY KEY (pymt_txn_id))";
 
     public static Set<String> cleanSchemaScript(){
         Set<String> scripts=new HashSet<String>();
