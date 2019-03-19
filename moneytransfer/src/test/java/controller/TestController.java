783fddbbac3a3e1c689c38934aa46d000c7c288b
zubair <zubair@heersoftware.com>
2019-03-19 08:20:24 +0530

    refractor and bug fix

--- moneytransfer/src/test/java/controller/TestController.java
+++ moneytransfer/src/test/java/controller/TestController.java
@@ -1,17 +1,13 @@
 package controller;
 
-import com.google.gson.Gson;
-import factory.DBConnectionFactory;
-import javafx.geometry.Pos;
-import model.MoneyTransferBook;
-import org.apache.http.HttpHeaders;
+
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Test;
 import java.io.IOException;
